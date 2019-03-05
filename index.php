2b20a1a5540bd04fb7d5134086d824162374ab7c
NikolFir <48108617+NikolFir@users.noreply.github.com>
2019-03-06 08:44:06 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("description", "1");
+$APPLICATION->SetPageProperty("keywords", "1");
+$APPLICATION->SetTitle("Кухни");
+?>
+<style>
+.catalog_item .catalog_item_name .series{font-size: 30px;}
+
+</style>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:catalog", 
+	"kitchens_new_design", 
+	array(
+		"AJAX_MODE" => "N",
+		"SEF_MODE" => "N",
+		"IBLOCK_TYPE" => "iblocks",
+		"IBLOCK_ID" => "4",
+		"USE_FILTER" => "N",
+		"USE_COMPARE" => "N",
+		"SHOW_TOP_ELEMENTS" => "Y",
+		"SECTION_COUNT_ELEMENTS" => "Y",
+		"SECTION_TOP_DEPTH" => "2",
+		"PAGE_ELEMENT_COUNT" => "30",
+		"LINE_ELEMENT_COUNT" => "3",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"LIST_PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"LIST_META_KEYWORDS" => "-",
+		"LIST_META_DESCRIPTION" => "-",
+		"LIST_BROWSER_TITLE" => "-",
+		"DETAIL_PROPERTY_CODE" => array(
+			0 => "options",
+			1 => "PROPS",
+			2 => "colors",
+			3 => "photos",
+			4 => "",
+		),
+		"DETAIL_META_KEYWORDS" => "-",
+		"DETAIL_META_DESCRIPTION" => "-",
+		"DETAIL_BROWSER_TITLE" => "-",
+		"BASKET_URL" => "/personal/basket.php",
+		"ACTION_VARIABLE" => "action",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"SECTION_ID_VARIABLE" => "SECTION_ID",
+		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"SET_TITLE" => "Y",
+		"SET_STATUS_404" => "N",
+		"PRICE_CODE" => array(
+		),
+		"USE_PRICE_COUNT" => "N",
+		"SHOW_PRICE_COUNT" => "1",
+		"PRICE_VAT_INCLUDE" => "Y",
+		"PRICE_VAT_SHOW_VALUE" => "N",
+		"PRODUCT_PROPERTIES" => array(
+		),
+		"USE_PRODUCT_QUANTITY" => "N",
+		"LINK_IBLOCK_TYPE" => "",
+		"LINK_IBLOCK_ID" => "",
+		"LINK_PROPERTY_SID" => "",
+		"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+		"USE_STORE" => "N",
+		"USE_ELEMENT_COUNTER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"PAGER_TITLE" => "Товары",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "Y",
+		"TOP_ELEMENT_COUNT" => "100",
+		"TOP_LINE_ELEMENT_COUNT" => "3",
+		"TOP_ELEMENT_SORT_FIELD" => "sort",
+		"TOP_ELEMENT_SORT_ORDER" => "asc",
+		"TOP_PROPERTY_CODE" => array(
+			0 => "",
+			1 => "ARTICLE",
+			2 => "",
+		),
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"COMPONENT_TEMPLATE" => "kitchens_new_design",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"USE_MAIN_ELEMENT_SECTION" => "N",
+		"DETAIL_STRICT_SECTION_CHECK" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_PROPERTIES_TO_BASKET" => "Y",
+		"PARTIAL_PRODUCT_PROPERTIES" => "N",
+		"TOP_ELEMENT_SORT_FIELD2" => "id",
+		"TOP_ELEMENT_SORT_ORDER2" => "desc",
+		"ELEMENT_SORT_FIELD2" => "id",
+		"ELEMENT_SORT_ORDER2" => "desc",
+		"SECTION_BACKGROUND_IMAGE" => "-",
+		"DETAIL_SET_CANONICAL_URL" => "N",
+		"DETAIL_CHECK_SECTION_ID_VARIABLE" => "N",
+		"DETAIL_BACKGROUND_IMAGE" => "-",
+		"SHOW_DEACTIVATED" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"COMPATIBLE_MODE" => "Y",
+		"DISABLE_INIT_JS_IN_COMPONENT" => "N",
+		"VARIABLE_ALIASES" => array(
+			"ELEMENT_ID" => "ELEMENT_ID",
+			"SECTION_ID" => "SECTION_ID",
+		)
+	),
+	false
+);?>
+<div>
+  <div></div>
+ </div>
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
